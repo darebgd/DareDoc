17f131833e6c219bd9c39a7bd1f7105b72043a85
darebgd <dare.bgd@gmail.com>
2021-10-18 22:57:20 +0200

    Extensions error #1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinxemoji.sphinxemoji','sphinx_tabs.tabs','sphinx_copybutton']
+extensions = ['sphinx_rtd_theme','sphinx_tabs.tabs','sphinx_copybutton']
+#, 'sphinxemoji.sphinxemoji','
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
