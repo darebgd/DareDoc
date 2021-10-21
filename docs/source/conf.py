908a53d73393167c2d6b748d4f82f194117236cb
darebgd <dare.bgd@gmail.com>
2021-10-21 12:47:35 +0200

    reveal "edot on Git"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx_rtd_theme']
 #, 'sphinxemoji.sphinxemoji','sphinx_tabs.tabs','sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,7 +49,7 @@ exclude_patterns = []
 # a list of builtin themes.
 import sphinx_rtd_theme
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'prev_next_buttons_location': 'None',    
@@ -62,15 +62,16 @@ html_theme_options = {
 
 html_style = 'css/my_theme.css' 
 
-html_show_sourcelink = False
-#html_js_files = ['js/expand_tabs.js']
+
+# html_show_sourcelink = False
+# html_js_files = ['js/expand_tabs.js']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_show_sphinx = False
+#html_show_sphinx = False
 
 rst_epilog = """
 
@@ -87,9 +88,9 @@ rst_epilog = """
 
 """
 
-html_last_updated_fmt = '%b %d, %Y'
-html_context = {
-"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
-"commit": False,
-}
+#html_last_updated_fmt = '%b %d, %Y'
+#html_context = {
+#"display_github": True, # Add 'Edit on Github' link instead of 'View page source'
+#"commit": False,
+#}
 
